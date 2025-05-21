20ceaa851a4aeb5ba09df7023865c00d1fb6a233
Anthon <anan0013@stud.kea.dk>
2025-05-21 21:19:59 +0200

    produktside billeder + footer tilføjet og start på popup cards

---
+++ js/produktpopup.js
@@ -0,0 +1,9 @@
+document.querySelector("#olandshveden").addEventListener("click", olandstext);
+document.querySelector("#hvedegraham").addEventListener("click", hvedegrahamtext);
+document.querySelector("#rug").addEventListener("click", rugtext);
+document.querySelector("#emmer").addEventListener("click", emmertext);
+document.querySelector("#guldblomme").addEventListener("click", guldblommetext);
+
+function open() {
+  console.log(this);
+}
